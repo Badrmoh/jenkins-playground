60df60b9a95b7095e54b81e8303de6085d2315a8
Badr Ibrahim <badrmoh@Badrs-MBP.fritz.box>
2022-06-26 15:43:30 +0200

    Create branch for Jenkins Shared Library

--- jenkins-shared-library/src/local/jenkins/constants/Constants.groovy
+++
@@ -1,7 +0,0 @@
-package local.jenkins.contants.Constants
-
-@Singleton
-class Constants{
-    static final String TEST_VAR1 = "First Variable";
-    static final String TEST_VAR2 = "Second Variable";
-}
